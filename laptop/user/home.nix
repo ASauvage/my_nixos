707ffcd94a9bf5c97c3e0c2e9707b8dd4f20d16a
ASauvage <a.sauvagepotel@gmail.com>
2025-03-26 18:37:40 +0100

    [feature] wip home.nix

--- laptop/user/home.nix
+++ laptop/user/home.nix
@@ -1,22 +1,45 @@
-{ inputs, pkgs, pkgs-unstable, pkgsags, userSettings, ... }: {
+{ inputs, pkgs, pkgs-unstable, pkgsags, userSettings, ... }:
+
+{
     imports = [
-        
+        ./hyprland/hyprland.nix
     ];
 
     home = {
         username = userSettings.username;
-        homeDirectory = "/home/${userSettings.username}";
+        homeDirectory = "/home/" + userSettings.username;
     };
 
-    # Add stuff for your user as you see fit:
-    # programs.neovim.enable = true;
+    programs.home-manager.enable = true;
+
     home.packages = with pkgs; [
         steam
     ];
 
-    # Enable home-manager and git
-    programs.home-manager.enable = true;
-    programs.git.enable = true;
+    xdg.enable = true;
+    xdg.userDirs = {
+        enable = true;
+        createDirectories = true;
+        music = "${config.home.homeDirectory}/Media/Music";
+        videos = "${config.home.homeDirectory}/Media/Videos";
+        pictures = "${config.home.homeDirectory}/Media/Pictures";
+        templates = "${config.home.homeDirectory}/Templates";
+        download = "${config.home.homeDirectory}/Downloads";
+        documents = "${config.home.homeDirectory}/Documents";
+        desktop = null;
+        publicShare = null;
+        extraConfig = {
+            XDG_DOTFILES_DIR = "${config.home.homeDirectory}/.dotfiles";
+            XDG_ARCHIVE_DIR = "${config.home.homeDirectory}/Archive";
+        };
+    };
+
+    home.sessionVariables = {
+        EDITOR = userSettings.editor;
+        SPAWNEDITOR = userSettings.spawnEditor;
+        TERM = userSettings.term;
+        BROWSER = userSettings.browser;
+    };
 
     # Nicely reload system units when changing configs
     systemd.user.startServices = "sd-switch";

79d48acb34eefb61cdd52198af2b8ea14e82ae3b
ASauvage <a.sauvagepotel@gmail.com>
2025-03-30 21:40:52 +0200

    [cleanup] cleanup useless groups

--- laptop/system/configuration.nix
+++ laptop/system/configuration.nix
@@ -60,7 +60,7 @@
     users.users.${userSettings.username} = {
         isNormalUser = true;
         description = userSettings.name;
-        extraGroups = [ "networkmanager" "wheel" "input" "dialout" "video" "render" ];
+        extraGroups = [ "networkmanager" "wheel" "input"];
         packages = [];
         uid = 1000;
     };

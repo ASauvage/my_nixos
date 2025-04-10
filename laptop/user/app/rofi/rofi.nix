3930077e8fffae11dd3eb3aded2454e4680aa96d
Alex <a.sauvagepotel@gmail.com>
2025-04-10 10:15:09 +0200

    [feature] rofi configuration again

--- laptop/user/app/rofi/rofi.nix
+++ laptop/user/app/rofi/rofi.nix
@@ -7,14 +7,12 @@
         cycle = false;
         plugins = (with pkgs;[
             rofi-calc
+            rofi-emoji-wayland
         ]);
         extraConfig = {
             modi = "drun,filebrowser,calc";
         };
-        theme = ./custom_theme.rasi;
+        # theme = ./custom_theme.rasi;
     };
 
-    home.packages = (with pkgs; [
-        rofi-emoji-wayland
-    ]);
 }

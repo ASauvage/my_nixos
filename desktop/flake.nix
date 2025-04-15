f064d623a872e8ccb3ca7da00f8b54c5f0d2228c
ASauvage <a.sauvagepotel@gmail.com>
2025-04-15 14:21:23 +0200

    [config] generate lockfile

--- desktop/flake.nix
+++ desktop/flake.nix
@@ -1,5 +1,5 @@
 {
-    description = "NixOS system installation for laptop";
+    description = "NixOS system installation for desktop";
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
